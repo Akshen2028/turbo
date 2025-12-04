530706c5510110dce580a81bf6959cb245475f6b
Akshen Jasikumar <akshen.jasikumar@gmail.com>
2025-12-04 03:12:51 -0500

    Styled home page properly

--- turbo/Views/HomeView.swift
+++ turbo/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  turbo
 //
 //  Created by Akshen Jasikumar on 2025-10-15.
@@ -8,99 +8,200 @@
 import SwiftUI
 import UIKit
 import MessageUI
+
 struct HomeView: View {
+    
+    // MARK: - State Properties
     @State private var navigateTo = ""
     @State private var isActives = false
     @State private var isActive = false
     @State private var isActive1 = false
-    @State var show = false
-    @Environment(\.colorScheme) var colorScheme
-    @State var sug = false
+    @State private var show = false
+    @State private var sug = false
     @State private var showSheet = false
-    @State var result: Result<MFMailComposeResult, Error>? = nil
-    var itemsPerRow = 6
-    @State var isAnimating = false
-    @State var start = UnitPoint(x: 0, y: -2)
-    @State var end = UnitPoint(x: 4, y: 0)
-    let colors = [Color.white,Color.white,Color(red: 55/255, green: 213/255, blue: 209/255)]
+    @State private var result: Result<MFMailComposeResult, Error>? = nil
+    
+    @State private var isAnimating = false
+    @State private var start = UnitPoint(x: 0, y: -2)
+    @State private var end = UnitPoint(x: 4, y: 0)
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    // MARK: - Constants
+    let itemsPerRow = 6
+    let colors = [
+        Color.white,
+        Color.white,
+        Color(red: 55/255, green: 213/255, blue: 209/255)
+    ]
+    
+    
+    // MARK: - Body
     var body: some View {
-        NavigationView(){
-            ZStack{
-                LinearGradient(gradient: Gradient(colors: colors), startPoint: start, endPoint: end).ignoresSafeArea()
-                VStack{
-                    ForEach(0..<getNumberOfRows()){i in
-                        HStack{
-                            ForEach(0..<self.itemsPerRow){j in
-                                Image(self.getImage(indexLocation: (i * itemsPerRow) + j))
-                                    .resizable().scaledToFit().padding()
-                                    .frame(width:UIScreen.main.bounds.width/CGFloat(self.itemsPerRow), height: UIScreen.main.bounds.width/CGFloat(self.itemsPerRow))
-                                    .opacity(isAnimating ? 0.5: 0)
-                                    .animation(Animation.linear(duration: Double.random(in: 10.0...20.0)).repeatForever(autoreverses: true).delay(Double.random(in:0...5)))
-                            }
-                        }
+        NavigationView {
+            ZStack {
+                
+                // Background Gradient Animation
+                LinearGradient(
+                    gradient: Gradient(colors: colors),
+                    startPoint: start,
+                    endPoint: end
+                )
+                .ignoresSafeArea()
+                
+                // Animated Background Icons
+                animatedBackgroundIcons
+                
+                // Main Header + Buttons
+                mainButtons
+                
+                // Bottom About/Help Buttons
+                bottomButtons
+            }
+        }
+        .accentColor(.black)
+    }
+    
+    
+    // MARK: - Background Icons
+    private var animatedBackgroundIcons: some View {
+        VStack {
+            ForEach(0 ..< getNumberOfRows()) { row in
+                HStack {
+                    ForEach(0 ..< itemsPerRow) { col in
+                        Image(getImage(indexLocation: (row * itemsPerRow) + col))
+                            .resizable()
+                            .scaledToFit()
+                            .padding()
+                            .frame(
+                                width: UIScreen.main.bounds.width / CGFloat(itemsPerRow),
+                                height: UIScreen.main.bounds.width / CGFloat(itemsPerRow)
+                            )
+                            .opacity(isAnimating ? 0.5 : 0)
+                            .animation(
+                                Animation.linear(duration: Double.random(in: 10...20))
+                                    .repeatForever(autoreverses: true)
+                                    .delay(Double.random(in: 0...5)),
+                                value: isAnimating
+                            )
                     }
-                }.onAppear(){
-                    self.isAnimating = true
                 }
-                VStack{
-                    Text("Talkaholic").fontWeight(.bold).foregroundColor(.black).font(.system(size: 38))
-                    NavigationLink(destination: CategoryListView()){
-                        Text("Select Category").fontWeight(.bold).foregroundColor(.white).padding(20.0).font(.system(size: 20))
-                    }
-                    .background(Color(red: 55/255, green: 213/255, blue: 209/255))
-                    .cornerRadius(80.0)
-                    .shadow(radius: 10)
-                    
-                    Button(action: {
-                        self.suggestFeature()
-                        sug = false
-                    }) {
-                        Text("Send Questions").fontWeight(.bold).foregroundColor((Color(red: 55/255, green: 213/255, blue: 209/255))).padding(20.0).font(.system(size: 20))
-                    }
-                    .background(Color.white)
-                    .cornerRadius(80.0)
-                    .shadow(radius: 10)
-                    .padding(4.0)
-                    .sheet(isPresented: $showSheet) {
-                        if sug{
-                        MailView(result: self.$result, newSubject: "New Feature Suggestion", newMsgBody: "I am enjoying this app, but I want to suggest a new feature!!!\n\nSuggestion:")
-                        }else{
-                        MailView(result: self.$result, newSubject: "New Question Suggestion", newMsgBody: "I am enjoying this app, but I want to send a new Question!!!\n\nCategory: \n\nQuestion: ")
-                        }
-                    }
-                    Spacer().frame(height:200)
+            }
+        }
+        .onAppear {
+            isAnimating = true
+        }
+    }
+    
+    
+    // MARK: - Main Buttons & Title
+    private var mainButtons: some View {
+        VStack {
+            Text("Talkaholic")
+                .font(.system(size: 38, weight: .bold))
+                .foregroundColor(.black)
+            
+            // Select Category
+            NavigationLink(destination: CategoryListView()) {
+                Text("Select Category")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding(20)
+            }
+            .background(Color(red: 55/255, green: 213/255, blue: 209/255))
+            .cornerRadius(80)
+            .shadow(radius: 10)
+            
+            // Send Questions
+            Button(action: {
+                suggestFeature()
+                sug = false
+            }) {
+                Text("Send Questions")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color(red: 55/255, green: 213/255, blue: 209/255))
+                    .padding(20)
+            }
+            .background(Color.white)
+            .cornerRadius(80)
+            .shadow(radius: 10)
+            .padding(.top, 4)
+            .sheet(isPresented: $showSheet) {
+                if sug {
+                    MailView(
+                        result: $result,
+                        newSubject: "New Feature Suggestion",
+                        newMsgBody: "I am enjoying this app, but I want to suggest a new feature!!!\n\nSuggestion:"
+                    )
+                } else {
+                    MailView(
+                        result: $result,
+                        newSubject: "New Question Suggestion",
+                        newMsgBody: "I am enjoying this app, but I want to send a new Question!!!\n\nCategory: \n\nQuestion: "
+                    )
                 }
-                VStack{
-                    Spacer()
-                    HStack{
-                        NavigationLink(destination: AboutView()){
-                            Image(systemName:"info.circle.fill").resizable().scaledToFit().frame(width:40).padding(-7).background(Color.white).padding(13).cornerRadius(20).shadow(radius: 12).foregroundColor(Color(red: 55/255, green: 213/255, blue: 209/255))
-                        }
-                        NavigationLink(destination: HelpView()){
-                            Image(systemName:"questionmark.circle.fill").resizable().scaledToFit().frame(width:40).padding(-7).background(Color(red: 55/255, green: 213/255, blue: 209/255)).padding(13).cornerRadius(20).shadow(radius: 12).foregroundColor(Color.white).padding(.leading, 0)
-                        }
-                    }
-                    Spacer().frame(height: 200)
+            }
+            
+            Spacer().frame(height: 200)
+        }
+    }
+    
+    
+    // MARK: - Bottom Info & Help Buttons
+    private var bottomButtons: some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                
+                NavigationLink(destination: AboutView()) {
+                    Image(systemName: "info.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40)
+                        .padding(-7)
+                        .background(Color.white)
+                        .padding(13)
+                        .cornerRadius(20)
+                        .shadow(radius: 12)
+                        .foregroundColor(Color(red: 55/255, green: 213/255, blue: 209/255))
                 }
                 
+                NavigationLink(destination: HelpView()) {
+                    Image(systemName: "questionmark.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40)
+                        .padding(-7)
+                        .background(Color(red: 55/255, green: 213/255, blue: 209/255))
+                        .padding(13)
+                        .cornerRadius(20)
+                        .shadow(radius: 12)
+                        .foregroundColor(.white)
+                }
             }
-        }.accentColor( .black)
+            
+            Spacer().frame(height: 200)
+        }
     }
-    func getImage(indexLocation:Int) -> String{
+    
+    
+    // MARK: - Helper Functions
+    func getImage(indexLocation: Int) -> String {
         return String(indexLocation % 2)
     }
     
-    func getNumberOfRows() -> Int{
-        let heightPerItem = UIScreen.main.bounds.width/CGFloat(self.itemsPerRow)
-        return Int(UIScreen.main.bounds.height/heightPerItem) + 1
+    func getNumberOfRows() -> Int {
+        let heightPerItem = UIScreen.main.bounds.width / CGFloat(itemsPerRow)
+        return Int(UIScreen.main.bounds.height / heightPerItem) + 1
     }
+    
     func suggestFeature() {
         print("Hurray! New Suggestion")
         if MFMailComposeViewController.canSendMail() {
-            self.showSheet = true
+            showSheet = true
         } else {
             print("Error sending mail")
-            // Alert : Unable to send the mail
         }
     }
 }
