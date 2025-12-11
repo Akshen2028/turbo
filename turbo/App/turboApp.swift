e3bf05daaf44b7b45040c25df897013f78164610
Akshen Jasikumar <akshen.jasikumar@gmail.com>
2025-12-11 04:05:55 -0500

    Fixed nav bar styling

--- turbo/App/turboApp.swift
+++ turbo/App/turboApp.swift
@@ -11,6 +11,23 @@ import CoreData
 @main
 struct turboApp: App {
     let persistenceController = PersistenceController.shared
+    
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.titleTextAttributes = [
+            .font: UIFont.systemFont(ofSize: 24, weight: .bold),
+            .foregroundColor: UIColor.black
+        ]
+        appearance.largeTitleTextAttributes = [
+            .font: UIFont.systemFont(ofSize: 32, weight: .bold),
+            .foregroundColor: UIColor.black
+        ]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().tintColor = .black
+    }
 
     var body: some Scene {
         WindowGroup {
